82e83845fb64178ff12eac375f22a317aed9441c
Tekla Inashvili <tekla.inashvili.1@btu.edu.ge>
2022-08-11 13:51:54 +0400

    first commit

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-console.log('get started');
-
 const add = function(a, b) {
 	return Number(a) + Number(b);
 };
@@ -25,115 +23,7 @@ const operate = function(a, b, sign) {
     if(sign == '/') return divide(a,b);
 }
 
-// console.log(operate(1, 2, '/'));
-
-// ---------------------
-
-// numbers.addEventListener('click' , console.log('ds'))
-
-
-// let numsToOpperandOn = [];
-// numbers.forEach(el => {
-//     // const display = document.querySelector('.display');
-
-//     el.addEventListener('click',() => { 
-//         const keyContent = el.textContent
-//         const displayedNum = display.textContent;
-
-//         if (displayedNum == '0') {
-//             display.textContent = keyContent
-//         } else {
-//             display.textContent = displayedNum + keyContent
-//         }
-//         // console.log(el.textContent)
-//         // display.textContent = el.textContent;
-//     });
-// })
-
-// let operandsArr = [];
-// operands.forEach(el => {
-//     el.addEventListener('click',() => { console.log(el.textContent)
-//         operandsArr.push(el.textContent);
-//         display.textContent = el.textContent;
-//     });
-// })
-
-//შემოვიღო ცარიელი სტრინგი და როცა მომხმარებელი დააჭერს რიცხვს ხელს,
-//დავუმატო ეს რიცხვ(ებ)ი სტრინგს იქამდე, სანამ მომხამრებელი დააჭერს რომელიმე 
-//ოპერანდს.
-//ამის შემდეგ ეს საბოლოოდ მიღებული სტრიგი გარდავქმნა ინტად 
-//და დავუმატო რიცხვების ერეის.
-
-//get first number
-
-
-
-
-
-// let numArrSec = [];
-
-
-// function getNum () {
-//     let numArrFirst = [];
-//     // console.log('shemovida 1');
-//     numbers.forEach(each => {
-//         // console.log('shemovida 2');
-//         each.addEventListener('click', () => {
-//             numArrFirst.push(each.textContent);
-//             console.log(numArrFirst);
-//             console.log('shemovida 3');
-//         });
-//         // console.log('shemovida 4');
-//     });
-//     // console.log('shemovida 5');
-//     return numArrFirst.join('');
-// }
-
-// numbers.forEach(each => {
-//     each.addEventListener('click', () => {
-//         numArrSec.push(each.textContent);
-//         console.log(numArrSec);
-//     });
-// });
-
-// operands.forEach(each => {
-//     each.addEventListener('click', () => {
-//         let sign = each.textContent;
-//         let firstNum = getNum();
-//         let secNum = getNum();
-
-//         console.log(firstNum + ' ' + sign + ' ' + secNum);
-        
-//         // console.log(numArrFirst);
-//     })
-// })
-
-
-// getNum();
-//როცა დაეჭირება ოპერანდ ბათონს, მეორე რიცხვი უნდა შევინახოთ 
-//პირველის ანალოგური მეთოდით და მერე უნდა მოხდეს ოპერიაციის ფუნქციის გამოძახება 
-//ამ ორ რიცხვზე და ერთ ნიშანზე. ამ ფუნქციის შედეგს textContentით დავწერთ დისპლეიზე
-
-// equalBtn.addEventListener('click', () => {
-//     let a = numsToOpperandOn.slice(-2)[0]; 
-//     let b = numsToOpperandOn.slice(-2)[1]; 
-//     let sign = operandsArr.slice(-1);
-
-//     display.textContent = operate(a, b, sign);
-// })
-
-
-
-
-
-
-
-
-
-
-
-
-
+// ------------
 
 
 
@@ -145,37 +35,35 @@ const display = document.querySelector('.display');
 const operands = document.querySelectorAll('.operands');
 const equalBtn = document.querySelector('.equal');
 
+
 const calculator = document.querySelector('.calculator');
 const keys = document.querySelector('.calculator__keys');
 const numberArr = [];
-console.log(keys);
+
+
 keys.addEventListener('click', e => {
+
     if (e.target.matches('button')) {
+        
         const key = e.target
         const action = key.dataset.action
-    const previousKeyType = calculator.dataset.previousKeyType
-
+        const previousKeyType = calculator.dataset.previousKeyType
         const keyContent = key.textContent
         const displayedNum = display.textContent;
 
-        
-        // console.log(el.textContent)
-        // display.textContent = el.textContent;
-        if(!action) {
-            console.log('num');
-            console.log(keyContent);
-            if (displayedNum === '0') {
-                display.textContent = keyContent
-            } else if (displayedNum === '0' || previousKeyType === 'operator') {
-                      display.textContent = keyContent;
-                    } 
-                    else {
-                        console.log('displayed ' + displayedNum + ' key ' + keyContent);
-                      display.textContent = displayedNum + keyContent;
-                    }
+        if (!action) {
+            if (displayedNum === '0' || previousKeyType === 'operator') {
+                display.textContent = keyContent;
+                delete calculator.dataset.previousKeyType;
+
+                console.log(calculator.dataset);
+            } else {
+                display.textContent = displayedNum + keyContent;
+                delete calculator.dataset.operator;
+
+            }
         }
 
-        // let numberArr = [];
         if (
               action === 'add' ||
               action === 'subtract' ||
@@ -185,45 +73,20 @@ keys.addEventListener('click', e => {
                 numberArr.push(display.textContent);
                 // console.log();
                 console.log(keyContent);
-            //   key.classList.add('is-depressed')
-              // Add custom attribute
-              calculator.dataset.previousKeyType = 'operator';
-              display.textContent = keyContent;
-
-                // display.textContent = '0';
-
+                calculator.dataset.previousKeyType = 'operator';
+                display.textContent = keyContent;
             }
+
+
             console.log(numberArr);
     }
    })
 
 
-///წამოვიღოთ ყველა ბათონი, 
-//const key = e.target
-//const action = key.dataset.action
-
-//შედმდეგ ვნახოთ, თუ ამ ბათონებს ექშენ აქვთ. თუ არ აქვს, ანუ რიცხვებია
-//თუ აქვთ, ანუ ნიშნები
-
-//შემდეგ რიცხვები მაქვს, 
-//შემდეგ თუ if (
-    //   action === 'add' ||
-    //   action === 'subtract' ||
-    //   action === 'multiply' ||
-    //   action === 'divide'
-    // ) {
-    //   key.classList.add('is-depressed')
-    //   // Add custom attribute
-    //   calculator.dataset.previousKeyType = 'operator'
-    // }
-
-    //const previousKeyType = calculator.dataset.previousKeyType
-
-// if (!action) {
-//     if (displayedNum === '0' || previousKeyType === 'operator') {
-//       display.textContent = keyContent
-//     } else {
-//       display.textContent = displayedNum + keyContent
-//     }
-//   }
-  
+   //ახლა დისპლეიზე ზემოთ შემოვიღოთ ცალკე პარაგრაფი, 
+   //სადაც სრული ისტორია ჩაეწერება, ანუ 45 + 45 მაგალითად
+   //და ოპერანდებთან შემოვიღოთ იფ სტეიტმენტი, 
+   //რომ თუ იმ ერეის სიგრძე იყო 2, უკვე მოხდეს მათემატიკური ოპერაცია,
+   //შედეგი გამოისახოს დისპლეიზე, გასუფთავდეს ერეი 
+   //და ჩაიწეროს შედეგი ნულოვან ინდექსზე.
+   //
